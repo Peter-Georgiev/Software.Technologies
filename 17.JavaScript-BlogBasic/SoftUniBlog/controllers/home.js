45bbd7fed5cecc85267fcf5fc05511d355dc778a
Peter Georgiev <peroxmen@hotmail.com>
2017-09-17 21:15:41 +0300

    Fix bug sort('-data')

--- 17.JavaScript-BlogBasic/SoftUniBlog/controllers/home.js
+++ 17.JavaScript-BlogBasic/SoftUniBlog/controllers/home.js
@@ -2,7 +2,7 @@ const Article = require('mongoose').model('Article');
 
 module.exports = {
   index: (req, res) => {
-      Article.find({}).limit(6).populate('author').then(articles => {
+      Article.find({}).sort('-data').limit(6).populate('author').then(articles => {
           res.render('home/index', {articles: articles})
       });
   }
